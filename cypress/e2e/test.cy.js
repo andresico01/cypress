eb269c40ba80affdf22bdd415875801cf19fa54c
andresico01 <andresrojasdg2.0@gmail.com>
2026-03-25 17:12:33 -0500

    add test

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -18,17 +18,14 @@ describe('Prueba de tareas con variables de ambiente', () => {
 
 it('entramos en pag principal de conversor de monedas',()=>{
             
-        MoneyTask.setMony(UI.ENTRADA_DINERO,AMOUNT);
-        MoneyTask.enter(UI.ENTER_BOTON);
 
         let expectedAmount = BALANCE_TOTAL - AMOUNT; 
-
+        MoneyTask.setMony(UI.ENTRADA_DINERO,AMOUNT);
+        MoneyTask.enter(UI.ENTER_BOTON);
         cy.get(UI.TEXTO_EXITOSO).should('be.visible');
         cy.get(UI.MESSAGE_SUCCESS).should('have.text', '¡Conversión Exitosa!');
-        cy.get(UI.CONVERSION_RESULT).should('contain.text', `Monto convertido: ${expectedAmount.toFixed(2)} COP`);
+        cy.get(UI.CONVERSION_RESULT).should('have.text', `Nuevo balance: ${expectedAmount} USDC`);
         
-
-
      
         });
 
